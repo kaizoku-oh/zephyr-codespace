63c3639127320d99e69887ba9728deb2c3a7be1a
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-04-20 16:49:58 +0000

    cleaning up fat

--- scripts/deploy.sh
+++
@@ -1,2 +0,0 @@
-sudo rm -rf /var/www/html/zephyr.signed.bin
-sudo cp ../build/zephyr/zephyr.signed.bin /var/www/html/
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,10 +16,6 @@ west zephyr-export
 echo "[setup.sh] Installing Python dependencies required by Zephyr"
 pip install -r deps/zephyr/scripts/requirements.txt
 
-# Install Python dependencies required by MCUBoot
-echo "[setup.sh] Installing Python dependencies required by MCUBoot"
-pip install -r deps/bootloader/mcuboot/scripts/requirements.txt
-
 # Copy .vscode to outer directory
 echo "[setup.sh] Copying .vscode to outer workspace directory"
 if [ -d ".vscode" ]; then
