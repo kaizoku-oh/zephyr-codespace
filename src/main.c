a35fcc81a43a1988fc9eb740ef7a9acfe0b49a8c
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-07 13:13:42 +0000

    Update code

--- src/main.c
+++ src/main.c
@@ -18,9 +18,11 @@ LOG_MODULE_REGISTER(main);
   * @param  None
   * @retval None
   */
-int main(void) {
-  while (true) {
-    k_msleep(300);
+int main(void)
+{
+  while (true)
+  {
+    k_msleep(500);
   }
 
   return EXIT_FAILURE;
@@ -32,42 +34,52 @@ int main(void) {
   * @param  context Exception context, with details and partial or full register
   * @retval None
   */
-void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *context) {
-  struct k_thread *faultingThread = NULL;
+void k_sys_fatal_error_handler(unsigned int reason, const struct arch_esf *context)
+{
+  struct k_thread *faulting_thread = NULL;
 
   switch (reason) {
-    case K_ERR_CPU_EXCEPTION: {
+    case K_ERR_CPU_EXCEPTION:
+    {
       LOG_ERR("Generic CPU exception, not covered by other codes");
       break;
     }
-    case K_ERR_SPURIOUS_IRQ: {
+    case K_ERR_SPURIOUS_IRQ:
+    {
       LOG_ERR("Unhandled hardware interrupt");
       break;
     }
-    case K_ERR_STACK_CHK_FAIL: {
+    case K_ERR_STACK_CHK_FAIL:
+    {
       LOG_ERR("Faulting context overflowed its stack buffer");
       /* Get the current thread that caused the fault */
-      faultingThread = k_current_get();
-      if (faultingThread) {
-        LOG_ERR("Fault occurred in thread: %s", k_thread_name_get(faultingThread));
-        LOG_ERR("Thread ID: %p", (void *)faultingThread);
+      faulting_thread = k_current_get();
+      if (faulting_thread)
+      {
+        LOG_ERR("Fault occurred in thread: %s", k_thread_name_get(faulting_thread));
+        LOG_ERR("Thread ID: %p", (void *)faulting_thread);
         LOG_ERR("Stack start: %p, size: %zu",
-                (void *)faultingThread->stack_info.start,
-                faultingThread->stack_info.size);
-      } else {
+                (void *)faulting_thread->stack_info.start,
+                faulting_thread->stack_info.size);
+      }
+      else
+      {
         LOG_ERR("Could not determine faulting thread");
       }
       break;
     }
-    case K_ERR_KERNEL_OOPS: {
+    case K_ERR_KERNEL_OOPS:
+    {
       LOG_ERR("High severity software error");
       break;
     }
-    case K_ERR_ARCH_START: {
+    case K_ERR_ARCH_START:
+    {
       LOG_ERR("Arch specific fatal errors");
       break;
     }
-    default: {
+    default:
+    {
       LOG_ERR("Unknow reason for fatal error (%d)", reason);
       break;
     }
