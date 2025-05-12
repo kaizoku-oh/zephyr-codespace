f57d54a28c2051d137b089293c383dbf8214ef77
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-12 08:52:02 +0000

    Integrating the pn532 driver

--- src/main.c
+++ src/main.c
@@ -7,8 +7,16 @@
 
 // Zephyr includes
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
+
+#include <pn532.h>
+
+/*-----------------------------------------------------------------------------------------------*/
+/* Private function prototypes                                                                   */
+/*-----------------------------------------------------------------------------------------------*/
+static void pn532_example(void);
 
 /*-----------------------------------------------------------------------------------------------*/
 /* Public functions                                                                              */
@@ -20,6 +28,8 @@ LOG_MODULE_REGISTER(main);
   */
 int main(void)
 {
+  pn532_example();
+
   while (true)
   {
     k_msleep(500);
@@ -83,5 +93,35 @@ void k_sys_fatal_error_handler(unsigned int reason, const struct arch_esf *conte
       LOG_ERR("Unknow reason for fatal error (%d)", reason);
       break;
     }
+  }
+}
+
+/*-----------------------------------------------------------------------------------------------*/
+/* Private functions                                                                             */
+/*-----------------------------------------------------------------------------------------------*/
+/**
+  * @brief  Example of using the PN532 NFC driver
+  * @retval None
+  */
+static void pn532_example(void)
+{
+  const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532_i2c);
+  uint32_t fw_version = 0;
+
+  if (!device_is_ready(dev))
+  {
+    LOG_INF("PN532 device not ready");
+    return;
+  }
+
+  if (pn532_get_fw_version(dev, &fw_version) == 0)
+  {
+    LOG_INF("PN532 Firmware Version: %02X.%02X",
+            (fw_version >> 8) & 0xFF,
+            fw_version & 0xFF);
+  }
+  else
+  {
+    LOG_ERR("Failed to get PN532 firmware version");
   }
 }
